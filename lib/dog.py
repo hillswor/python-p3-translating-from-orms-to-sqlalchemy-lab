15a96232d8dabdc52388b416c97b7b15eef413eb
Bruce <bruce.hillsworth@gmail.com>
2023-05-10 19:41:50 -0600

    Still have a few more tests to go

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,33 @@
-from models import Dog
+from models import Base, Dog
+import ipdb
 
-def create_table(base):
-    pass
+
+def create_table(base, engine):
+    base.metadata.create_all(engine)
+
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    pass
+    query = session.query(Dog).filter(Dog.name == name)
+    return query.first()
+
 
 def find_by_id(session, id):
-    pass
+    query = session.query(Dog).filter(Dog.id == id)
+    return query.first()
+
 
 def find_by_name_and_breed(session, name, breed):
     pass
+
 
 def update_breed(session, dog, breed):
     pass
